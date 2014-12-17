20686f90804992dd782db502f6c3d46bfdb2a4be
wobushigogo <wangy322@sina.com>
2014-12-17 08:57:15 +0800

    Initial Commit

---
+++ LoginApi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginApi
+//
+//  Created by 汪洋 on 14/12/17.
+//  Copyright (c) 2014年 wy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
